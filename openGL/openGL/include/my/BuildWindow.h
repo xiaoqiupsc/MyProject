bb2a3003ec13fa069f1589a56e90ad9310c52a20
nhljh1234 <1104316817@qq.com>
2018-10-24 13:57:34 +0800

    整理代码

--- openGL/openGL/include/my/BuildWindow.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-//创建一个窗口
-GLFWwindow* createOpenGLWindow(bool useAntiAliasing = false, int samplesNum = 4);
-//窗口关闭的时候清除缓冲
-void closeWindow();
